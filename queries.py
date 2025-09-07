d08e814a0573564f812fa329e7a98e1ab593ee36
Bishal Raj Basumatary <bishal.rajbb@gmail.com>
2025-09-08 04:07:15 +0530

    Update queries.py

--- queries.py
+++ queries.py
@@ -1,12 +1,18 @@
 import os
 import pandas as pd
-import psycopg2
-from config import DB_CONFIG
+from config import DB_CONFIG, USE_SQLITE, DB_PATH
 
 def run_query(sql):
-    conn = psycopg2.connect(**DB_CONFIG)
-    df = pd.read_sql_query(sql, conn)
-    conn.close()
+    if USE_SQLITE:
+        import sqlite3
+        conn = sqlite3.connect(DB_PATH)
+        df = pd.read_sql_query(sql, conn)
+        conn.close()
+    else:
+        import psycopg2
+        conn = psycopg2.connect(**DB_CONFIG)
+        df = pd.read_sql_query(sql, conn)
+        conn.close()
     return df
 
 def total_sales():
@@ -14,37 +20,47 @@ def total_sales():
     return run_query(sql)
 
 def monthly_sales():
-    sql = """
-SELECT TO_CHAR(TO_DATE(order_date, 'YYYY-MM-DD'), 'YYYY-MM') AS month, 
-       SUM(oi.quantity * oi.unit_price) AS monthly_sales
-FROM orders o
-JOIN order_items oi ON o.order_id = oi.order_id
-GROUP BY TO_CHAR(TO_DATE(order_date, 'YYYY-MM-DD'), 'YYYY-MM')
-ORDER BY month;
-"""
+    if USE_SQLITE:
+        sql = """
+        SELECT substr(order_date,1,7) AS month, 
+               SUM(oi.quantity * oi.unit_price) AS monthly_sales
+        FROM orders o
+        JOIN order_items oi ON o.order_id = oi.order_id
+        GROUP BY month
+        ORDER BY month;
+        """
+    else:
+        sql = """
+        SELECT TO_CHAR(TO_DATE(order_date, 'YYYY-MM-DD'), 'YYYY-MM') AS month, 
+               SUM(oi.quantity * oi.unit_price) AS monthly_sales
+        FROM orders o
+        JOIN order_items oi ON o.order_id = oi.order_id
+        GROUP BY TO_CHAR(TO_DATE(order_date, 'YYYY-MM-DD'), 'YYYY-MM')
+        ORDER BY month;
+        """
     return run_query(sql)
 
 def top_products(limit=5):
     sql = f"""
-SELECT p.name AS product, SUM(oi.quantity * oi.unit_price) AS revenue
-FROM products p
-JOIN order_items oi ON p.product_id = oi.product_id
-GROUP BY p.name
-ORDER BY revenue DESC
-LIMIT {limit};
-"""
+    SELECT p.name AS product, SUM(oi.quantity * oi.unit_price) AS revenue
+    FROM products p
+    JOIN order_items oi ON p.product_id = oi.product_id
+    GROUP BY p.name
+    ORDER BY revenue DESC
+    LIMIT {limit};
+    """
     return run_query(sql)
 
 def top_customers(limit=5):
     sql = f"""
-SELECT c.name AS customer, SUM(oi.quantity * oi.unit_price) AS spending
-FROM customers c
-JOIN orders o ON c.customer_id = o.customer_id
-JOIN order_items oi ON o.order_id = oi.order_id
-GROUP BY c.name
-ORDER BY spending DESC
-LIMIT {limit};
-"""
+    SELECT c.name AS customer, SUM(oi.quantity * oi.unit_price) AS spending
+    FROM customers c
+    JOIN orders o ON c.customer_id = o.customer_id
+    JOIN order_items oi ON o.order_id = oi.order_id
+    GROUP BY c.name
+    ORDER BY spending DESC
+    LIMIT {limit};
+    """
     return run_query(sql)
 
 if __name__ == '__main__':
