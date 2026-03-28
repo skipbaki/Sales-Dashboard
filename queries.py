aa90d73637e06a56dbc9965f5aa2351ef5fd5c71
Bishal Raj Basumatary <bishal.rajbb@gmail.com>
2026-03-28 18:55:21 +0530

    fixed sql query for formatting

--- queries.py
+++ queries.py
@@ -21,38 +21,46 @@ def total_sales():
 
 def monthly_sales():
     if USE_SQLITE:
-        sql = """ SELECT substr(order_date,1,7) AS month, 
+        sql = """
+        SELECT substr(order_date,1,7) AS month, 
                SUM(oi.quantity * oi.unit_price) AS monthly_sales
         FROM orders o
         JOIN order_items oi ON o.order_id = oi.order_id
         GROUP BY month
-        ORDER BY month; """
+        ORDER BY month;
+        """
     else:
-        sql = """ SELECT TO_CHAR(TO_DATE(order_date, 'YYYY-MM-DD'), 'YYYY-MM') AS month, 
+        sql = """
+        SELECT TO_CHAR(TO_DATE(order_date, 'YYYY-MM-DD'), 'YYYY-MM') AS month, 
                SUM(oi.quantity * oi.unit_price) AS monthly_sales
         FROM orders o
         JOIN order_items oi ON o.order_id = oi.order_id
         GROUP BY TO_CHAR(TO_DATE(order_date, 'YYYY-MM-DD'), 'YYYY-MM')
-        ORDER BY month; """
+        ORDER BY month;
+        """
     return run_query(sql)
 
 def top_products(limit=5):
-    sql = f""" SELECT p.name AS product, SUM(oi.quantity * oi.unit_price) AS revenue
+    sql = f"""
+    SELECT p.name AS product, SUM(oi.quantity * oi.unit_price) AS revenue
     FROM products p
     JOIN order_items oi ON p.product_id = oi.product_id
     GROUP BY p.name
     ORDER BY revenue DESC
-    LIMIT {limit}; """
+    LIMIT {limit};
+    """
     return run_query(sql)
 
 def top_customers(limit=5):
-    sql = f""" SELECT c.name AS customer, SUM(oi.quantity * oi.unit_price) AS spending
+    sql = f"""
+    SELECT c.name AS customer, SUM(oi.quantity * oi.unit_price) AS spending
     FROM customers c
     JOIN orders o ON c.customer_id = o.customer_id
     JOIN order_items oi ON o.order_id = oi.order_id
     GROUP BY c.name
     ORDER BY spending DESC
-    LIMIT {limit}; """
+    LIMIT {limit};
+    """
     return run_query(sql)
 
 if __name__ == '__main__':
@@ -61,4 +69,4 @@ if __name__ == '__main__':
     monthly_sales().to_csv(os.path.join(os.path.dirname(__file__), "outputs", "monthly_sales.csv"), index=False)
     top_products().to_csv(os.path.join(os.path.dirname(__file__), "outputs", "top_products.csv"), index=False)
     top_customers().to_csv(os.path.join(os.path.dirname(__file__), "outputs", "top_customers.csv"), index=False)
-    print("KPI CSV saved to outputs/")
+    print("KPI CSVs saved to outputs/")
